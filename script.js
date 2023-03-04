6b9debdee5b1f8f13ff07e58e08236c585cf2315
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-03-04 14:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const volume = player.querySelector('input[name="volume"]');
+const playbackRate = player.querySelector('input[name="playbackRate"]');
+
+function togglePlay() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+// Function to set the volume of the video
+function handleVolume() {
+  video.volume = this.value;
+}
+
+// Function to set the playback speed of the video
+function handlePlaybackRate() {
+  video.playbackRate = this.value;
+}
+
+// Function to skip forward or backward
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Event listeners for the video player and controls
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', togglePlay);
+video.addEventListener('pause', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+volume.addEventListener('input', handleVolume);
+playbackRate.addEventListener('input', handlePlaybackRate);
+skipButtons.forEach(button => button.addEventListener('click', skip));
